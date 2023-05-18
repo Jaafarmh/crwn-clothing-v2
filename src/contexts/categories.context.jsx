1330f4264fdb2f0001169fd1d1b516f55e05f6c1
jaafar mohammad <jafarmh777@gamil.com>
2023-05-18 21:56:58 +0200

    finishing Reducers

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -14,7 +14,7 @@ export const CategoriesProvider = ({ children }) => {
     const getCategoriesMap = async () => {
       const categoryMap = await getCategoriesAndDocuments();
       setCategoriesMap(categoryMap);
-    };
+    }; 
      getCategoriesMap();
   }, []);
 
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
+import { CreateAction } from '../utils/reducer/reducer.util';
 
 import {
   onAuthStateChangedListener,
@@ -10,11 +11,41 @@ export const UserContext = createContext({
   currentUser: null,
 });
 
-export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
+export const USER_ACTION_TYPES ={
+  SET_CURRENT_USER : 'SET_CURRENT_USER',
+}
 
-  useEffect(() => {
+const userReducer = (state,action) =>{
+const {type, payload} = action;
+
+  switch(type){
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+    return {
+      ...state,
+      currentUser: payload,
+    }
+    default :
+    throw new Error(`Unhandled type ${type} in userReducer`)
+  }
+}
+
+const INITIAL_STATE ={
+  currentUser : null,
+}
+
+export const UserProvider = ({ children }) => {
+const [state, dispatch] = useReducer(userReducer , INITIAL_STATE)
+const {currentUser} = state;
+
+const setCurrentUser = (user) =>{
+  dispatch(
+    CreateAction (USER_ACTION_TYPES.SET_CURRENT_USER, user))
+    
+}
+
+const value = {currentUser, setCurrentUser}
+
+useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) {
         createUserDocumentFromAuth(user);
